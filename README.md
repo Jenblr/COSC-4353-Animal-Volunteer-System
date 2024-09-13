ee9dd0dccc431096d560d7fe870174af63036d20
Jenblr <115195835+Jenblr@users.noreply.github.com>
2024-09-12 23:59:06 -0500

    Styled homepage of website with banner and mission statement

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 - Afra Nawar [afnawar910](https://github.com/afnawar910)
 - Jennifer Nguyen [Jenblr](https://github.com/Jenblr)
 - Jonathan Hsueh [Tuudou](https://github.com/tuudou)
-- Sofia Davila [Sofiad98] (https://github.com/Sofiad98)
+- Sofia Davila [Sofiad98](https://github.com/Sofiad98)
 
 # Packages to install
 - npm install
--- volunteer-system/src/components/Main/Taskbar.js
+++ volunteer-system/src/components/Main/Taskbar.js
@@ -2,23 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom'; // for SPA navigaiton
 import '../../styles/Taskbar.css';
 import logo from '../../images/AnimalShelterLogo.png'; 
+import firstBanner from '../../images/AnimalShelter_Home_Banner.png'; 
+import whoAreWe from '../../images/AnimalShelter_WhoAreWe.png';
 
 const Taskbar = () => {
     return (
-        <nav className="navbar">
-            <div className="logo-container">
-                <img src={logo} alt="Logo" className="logo" />
-                <span className="logo-text">Adopt-a-Companion Society</span>
+        <div>
+            <nav className="navbar">
+                <div className="logo-container">
+                    <img src={logo} alt="Logo" className="logo" />
+                    <span className="logo-text">Adopt-a-Companion Society</span>
+                </div>
+                <div className="taskbar">
+                    <ul>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/calendar'>Calendar</Link></li>
+                        <li><Link to='/profile'>Profile</Link></li>
+                        <li><Link to='/notifications'>Notifications</Link></li>
+                    </ul>
+                </div>
+            </nav>
+
+            {/* Banner styling under taskbar for home page*/}
+            <div className="banner-container">
+                <img src={firstBanner} alt="FirstBanner" className="firstBanner" />
             </div>
-            <div className="taskbar">
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/calendar'>Calendar</Link></li>
-                    <li><Link to='/profile'>Profile</Link></li>
-                    <li><Link to='/notifications'>Notifications</Link></li>
-                </ul>
+
+            {/* Who Are We statement */}
+            <div className="whoAreWe-container">
+                <img src={whoAreWe} alt="Who Are We" className="who-Are-We" />
             </div>
-        </nav>
+        </div>
     );
 };
 
