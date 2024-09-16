ddc301e09beef8bf3f5fd68626a8e5ac3b8f0a12
Tuudou <112034738+Tuudou@users.noreply.github.com>
2024-09-16 00:23:05 -0500

    Add files via upload

---
+++ VolunteerHistory/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VolunteerHistoryTable from './components/VolunteerHistoryTable';
+
+const App = () => {
+  return (
+    <div>
+      <VolunteerHistoryTable />
+    </div>
+  );
+};
+
+export default App;
